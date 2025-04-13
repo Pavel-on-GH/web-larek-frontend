cfc3cdb5bebfed769c9d60ac5db3802b795aabca
Pavel-on-GH <Pavel.Zaitsev-7@yandex.ru>
2025-04-13 15:49:42 +0300

    3.3.3.0 - Presenter: доработка Api

--- src/components/Presenter/AppApi.ts
+++ src/components/Presenter/AppApi.ts
@@ -6,34 +6,31 @@ export class AppApi {
 	private _baseApi: IApi;
 	private url: string;
 
-	constructor(cdn: string, baseApi: IApi) {
+	constructor(url: string, baseApi: IApi) {
 		this._baseApi = baseApi;
-		this.url = cdn;
+		this.url = url;
 	}
 
-	getProducts(): Promise<IProductList[]> {
-		return this._baseApi
-			.get<ApiListResponse<IProductList>>(`/product`)
-			.then((response) =>
-				response.items.map((product) => ({
-					...product,
-					image: this.url + product.image,
-				}))
-			);
+	async getProducts(): Promise<IProductList[]> {
+		const response = await this._baseApi.get<ApiListResponse<IProductList>>(
+			`/product`
+		);
+		return response.items.map((product) => ({
+			...product,
+			image: this.url + product.image,
+		}));
 	}
 
-	getProduct(id: string): Promise<IProductList> {
-		return this._baseApi
-			.get<IProductList>(`/product/${id}`)
-			.then((product) => ({
-				...product,
-				image: this.url + product.image,
-			}));
+	async getProduct(id: string): Promise<IProductList> {
+		const product = await this._baseApi.get<IProductList>(`/product/${id}`);
+		return {
+			...product,
+			image: this.url + product.image,
+		};
 	}
 
-	orderProducts(order: IOrder): Promise<IOrder> {
-		return this._baseApi
-			.post<IOrder>('/order', order)
-			.then((data: IOrder) => data);
+	async orderProducts(order: IOrder): Promise<IOrder> {
+		const data = await this._baseApi.post<IOrder>('/order', order);
+		return data;
 	}
 }
